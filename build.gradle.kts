ece6236f53127d065b0b059b4fe30af7296372f7
eugenashka <465730@niuitmo.ru>
2024-12-20 11:41:35 +0300

    restructed code

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.eugenashka"
-version = "1.1-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
     }
 
     signPlugin {
